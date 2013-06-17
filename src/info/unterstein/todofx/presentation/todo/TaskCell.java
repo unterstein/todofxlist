985e6898be74cdd61bcc083026504a89e2151e2e
Johannes Unterstein <unterstein@me.com>
2013-06-17 08:02:07 +0200

    new constructor

--- src/info/unterstein/todofx/presentation/todo/TaskCell.java
+++ src/info/unterstein/todofx/presentation/todo/TaskCell.java
@@ -2,13 +2,18 @@ package info.unterstein.todofx.presentation.todo;
 
 import info.unterstein.todofx.business.entity.Task;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.OverrunStyle;
 
 public class TaskCell extends ListCell<Task> {
+
+  public TaskCell() {
+    setTextOverrun(OverrunStyle.ELLIPSIS);
+  }
 
   @Override
   protected void updateItem(Task item, boolean empty) {
     super.updateItem(item, empty);
-    if (item != null) {
+    if (empty == false) {
       setText(item.getTitle());
     }
   }
